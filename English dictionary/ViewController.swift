f7fe0973184860dbda01713e8ab0098dadc01648
Hudihka <k.iroshnikov@itmegastar.com>
2020-11-15 16:01:00 +0700

    init commit

---
+++ English dictionary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  English dictionary
+//
+//  Created by Hudihka on 15/11/2020.
+//  Copyright © 2020 OOO MegaStar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
