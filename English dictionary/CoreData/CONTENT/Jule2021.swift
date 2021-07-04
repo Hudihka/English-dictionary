97360381eae5e62e9bd08442f2cfaa68a88deaca
hudihka <>
2021-07-04 20:27:55 +0700

    добавлены новые слова

---
+++ English dictionary/CoreData/CONTENT/Jule2021.swift
@@ -0,0 +1,46 @@
+//
+//  Jule2021.swift
+//  English dictionary
+//
+//  Created by Константин Ирошников on 04.07.2021.
+//  Copyright © 2021 OOO MegaStar. All rights reserved.
+//
+
+import Foundation
+
+class Jule2021 {
+    
+    public static let allContent: JSON = ["theme" : "2021 феврал-июнь", "allWords" : Jule2021.words]
+    
+    private static let words: [JSON] = [
+        ["engValue" : "cucombers", "rusValue" : "огурцы", "descript" : ""],
+        ["engValue" : "ripe", "rusValue" : "спеклый", "descript" : ""],
+        ["engValue" : "bars", "rusValue" : "батончик", "descript" : ""],
+        ["engValue" : "bitter", "rusValue" : "горький", "descript" : ""],
+        ["engValue" : "low-fat", "rusValue" : "низкокалорийный", "descript" : ""],
+        ["engValue" : "bowl", "rusValue" : "чашка", "descript" : ""],
+        ["engValue" : "food poisoned", "rusValue" : "пищевое отравление", "descript" : ""],
+        ["engValue" : "habit", "rusValue" : "привычка", "descript" : ""],
+        ["engValue" : "carrots", "rusValue" : "морковь", "descript" : ""],
+        ["engValue" : "lettuce", "rusValue" : "салат", "descript" : ""],
+        ["engValue" : "peas", "rusValue" : "горох", "descript" : ""],
+        ["engValue" : "crips", "rusValue" : "чипсы", "descript" : ""],
+        ["engValue" : "sweets", "rusValue" : "сладости", "descript" : ""],
+        ["engValue" : "sweet corn", "rusValue" : "сладкая кукуруза", "descript" : ""],
+        ["engValue" : "strict", "rusValue" : "строгий", "descript" : ""],
+        ["engValue" : "amusement", "rusValue" : "развлечение", "descript" : ""],
+        ["engValue" : "liquid", "rusValue" : "жидкость", "descript" : ""],
+        ["engValue" : "poverb", "rusValue" : "поговорка", "descript" : ""],
+        ["engValue" : "sad", "rusValue" : "расстроен", "descript" : ""],
+        ["engValue" : "cabbage", "rusValue" : "капуста", "descript" : ""],
+        ["engValue" : "horn", "rusValue" : "сигнал", "descript" : ""],
+        ["engValue" : "discuss", "rusValue" : "обсуждение", "descript" : ""],
+        ["engValue" : "puck", "rusValue" : "шайба", "descript" : ""],
+        ["engValue" : "to get", "rusValue" : "что бы получить", "descript" : ""],
+        ["engValue" : "to", "rusValue" : "куда", "descript" : ""],
+    
+    ]
+    
+}
+
+
